60ea2e0a864e4077f4406d7120a442c60555e9d2
kotC9 <krot.ik@ya.ru>
2019-12-20 17:43:54 +0500

    123

--- VueNancy.FrontEnd/src/components/Home.vue
+++ VueNancy.FrontEnd/src/components/Home.vue
@@ -1,24 +1,18 @@
 <template>
-    <d-container class='dr-example-container'>
-        <d-row>
-            <d-col cols='12' md='4' lg='3'></d-col>
-            <d-col cols='12' md='4' lg='6'>
+    <d-container class='action'>
+        <d-row justify='center'>
+            <div class='form-group'>
 
-                <div class='form-group'>
-                    <label class="sr-only" for="Login">Login</label>
-                    <d-input id="Login" class="mb-2 mr-sm-2 mb-sm-0" v-model="inputLogin" placeholder="Login" required />
-                </div>
+                <d-input id="Login" class="mb-2 mr-sm-2 mb-sm-0" v-model="inputLogin" placeholder="Login" required />
+            </div>
 
-                <div class='form-group'>
-                    <label for="PasswordInput" class="sr-only">Password</label>
-                    <d-form-input id="PasswordInput" type="password" v-model="inputPassword" required placeholder="Password"></d-form-input>
-                </div>
+            <div class='form-group'>
+                <d-form-input id="PasswordInput" type="password" v-model="inputPassword" required placeholder="Password"></d-form-input>
+            </div>
 
-                <div class='form-group'>
-                    <d-button pill theme='primary' @click='postLogin()'>Login</d-button>
-                </div>
-            </d-col>
-            <d-col cols='12' md='4' lg='3'></d-col>
+            <div class='form-group'>
+                <d-button pill theme='primary' @click='postLogin()'>Login</d-button>
+            </div>
         </d-row>
     </d-container>
 </template>
@@ -38,7 +32,7 @@
             };
         },
         methods: {
-            postLogin(e) {
+            postLogin() {
                 HTTP
                     .post('login', {
                         login: this.inputLogin,
@@ -52,9 +46,18 @@
                         }
                     })
                     .catch(error => {
-                        console.log(error);
+                        alert(error);
                     });
             }
         }
     };
 </script>
+
+<style>
+    .action {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 100vh;
+    }
+</style>
